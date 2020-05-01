0b53c4d7dfed568c67cbc06f267b369a2e994219
Audrey Castillo <valax321@gmail.com>
2020-05-02 07:31:50 +1000

    Shader bundle resource to ensure the shader assets always get built

---
+++ Effects/Runtime/ShaderBundle.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Valax321.PostProcess.Runtime
+{
+    #if PACKAGE_DEV
+    [CreateAssetMenu(menuName = "Valax321/Post Processing/Shader Bundle")]
+    #endif
+    public class ShaderBundle : ScriptableObject
+    {
+        [SerializeField] private Shader[] m_shaders;
+    }
+}
